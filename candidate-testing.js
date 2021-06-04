9db51fb808849db96c6877c0ea14552d34ad590b
Jason Wilson <>
2021-06-04 22:05:17 +0000

    Correct questions

--- candidate-testing.js
+++ candidate-testing.js
@@ -10,7 +10,7 @@ let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 let questions = ["Who was the first American woman in space? ","True or false: 5 kilometers = 5000 meters? ", "(5 + 3)/2 * 10 = ? ", "Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ", "What is the minimum crew size for the ISS? "];
-let correctAnswers = ["Sally Ride", "True", 40, "Trajectory", 3];
+let correctAnswers = ["Sally Ride", "True", "40", "Trajectory", "3"];
 let candidateAnswers = [];
 
 
@@ -30,10 +30,10 @@ function askQuestion() {
   // } else {
   //   console.log ("Sorry, that was incorrect.");
   // }
-  console.log("Your Answer: " + candidateAnswers[i]);
+  console.log("Your Answer: " + candidateAnswers);
   console.log("Correct Answer: " + correctAnswers[i]);
  }
-
+  
 }
 
 
